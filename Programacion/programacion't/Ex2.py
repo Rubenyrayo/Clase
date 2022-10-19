9e1425d6ae0183bd882d538cead14ba83e0d9168
Rubenyrayo <Rubenyrayo@hotmail.com>
2022-10-19 11:51:00 +0200

    nuevo html

--- Programacion/programacion't/Ex2.py
+++ Programacion/programacion't/Ex2.py
@@ -2,3 +2,14 @@
 Fes un programa que calculi el sou mensual d’un comercial. El sou base és de 1000 € i rep una comissió del 5% dels contractes que firmi.
 """
 
+soubase=(1000)
+contratobase=(200)
+
+contratosfirmados=int(input("Quantos contratos has firmado? "))
+nuevocontratobase=(contratobase * contratosfirmados)
+
+porcentage= 5 * nuevocontratobase/100
+
+nousou=(soubase + porcentage)
+
+print(nousou)
