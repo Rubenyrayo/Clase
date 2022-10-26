8652c7d3fa51e7f1ced0724554dc50e3731011fb
Rubenyrayo <Rubenyrayo@hotmail.com>
2022-10-26 10:15:09 +0200

    luego lo sigo

--- Programacion/programacion't/Ex3.py
+++ Programacion/programacion't/Ex3.py
@@ -10,36 +10,44 @@ Calculador de notes
 
 # Variables para guardar la info
 
+from os import link
+from timeit import repeat
+
+
 tasques=[]
 treballs=[]
 examen=[]
 
-# Valors per iniciar els bucles
-
-notatasques=()
-
 # Menu per accedir a cada part
 
-print("Apartats")
-print("\n""1. Tasques", "\n" "2. Treballs", "\n" "3. Treballs","\n")
-menus=int(input("A que part vols entrar? "))
-print("\n")
+while True:
+    print("Apartats")
+    print("\n""1. Tasques", "\n" "2. Treballs", "\n" "3. Treballs","\n" "4. Salir", "\n")
+    menus=int(input("A que part vols entrar? "))
+    print("\n")
 
-#Menu de tasques
-if menus==1:
-    print("Menu de tasques","\n")
-    print("Notes de tasques dipositades = ", tasques)
-    while True:
-        notatasques=float(input("Posa nota per nota les notes corresponents = "))
-        if notatasques==1:
-            break
-        else:
-            tasques.append(notatasques)
-            
-    print(notatasques, tasques)
-
-#Menu de treballs
-
+    #Menu de tasques
+    if menus==1:
+        print("Menu de tasques","\n")
+        print("Notes de tasques dipositades = ", tasques)
+        while True:
+            notatasques=float(input("Posa nota per nota les notes corresponents (per sortir escriu 100)= "))
+            if notatasques==100:
+                break
+            else:
+                tasques.append(notatasques)
+                
+    #Menu de treballs
+    if menus==2:
+            print("Menu de treballs","\n")
+            print("Notes de treballs dipositades = ", treballs)
+        while True:
+            notatreballs=float(input("Posa nota per nota les notes corresponents (per sortir escriu 100) = "))
+            if notatreballs==100:
+                break
+            else:
+                tasques.append(notatreballs)
+        print(notatreballs, tasques)
 
 
 
