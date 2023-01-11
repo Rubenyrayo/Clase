8e7438fd8139cf3628ebeece7e07031d73f6bf0a
Rubenyrayo <Rubenyrayo@hotmail.com>
2023-01-11 08:58:37 +0100

    a

---
+++ Programacion/Parte 2/Tres_en_ratlla.py
@@ -0,0 +1,12 @@
+"""
+Joc del tres en ratlla
+ 
+Autor: Ruben Delgado
+Data de creació: 11/1/23
+Data de modificació: 11/1/23
+"""
+
+import random
+
+print("1", "|", "2", "|", "3","\n","----------","\n","4", "|", "5", "|", "6","\n","----------","\n","7", "|", "8", "|", "9")
+
--- Programacion/programacion't/Exercicis/Ex1.py
+++ Programacion/programacion't/Exercicis/Ex1.py
@@ -1,15 +1,24 @@
-from audioop import reverse
+#from audioop import reverse
+#
+#
+#palabras=[]
+#
+#while True:
+#    palabra=input("Quina paraula vols?")
+#    if palabra == " ":
+#        break
+#    else:
+#        palabras.append(palabra)
+#    
+#for x in reversed(palabras):
+#    print(x)
+celsius = int(input())
 
+def conv(c):
+    #your code goes here
+    grados = 1.8 * c + 32
 
-palabras=[]
+    return grados
 
-while True:
-    palabra=input("Quina paraula vols?")
-    if palabra == " ":
-        break
-    else:
-        palabras.append(palabra)
-    
-for x in reversed(palabras):
-    print(x)
-    
+fahrenheit = conv(celsius)
+print(fahrenheit)
