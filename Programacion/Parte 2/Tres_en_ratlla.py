df4215fd1e463c30148c4a34bbb1e3a53197b7f0
Rubenyrayo <Rubenyrayo@hotmail.com>
2023-01-18 09:01:53 +0100

    tres en raya

--- Programacion/Parte 2/Tres_en_ratlla.py
+++ Programacion/Parte 2/Tres_en_ratlla.py
@@ -8,5 +8,24 @@ Data de modificació: 11/1/23
 
 import random
 
-print("1", "|", "2", "|", "3","\n","----------","\n","4", "|", "5", "|", "6","\n","----------","\n","7", "|", "8", "|", "9")
+def mostrartaulell(taulell):
+    taulell=[[" "," "," "],
+             [" "," "," "],
+             [" "," "," "]]
+    
+    for i in taulell:
+        print(i)
+        
+def posarfitxa():
+    and
+    
+def fijoc():
+    and
+    
+while True:
+    and
+
+
+
+print (mostrartaulell(1))
 
