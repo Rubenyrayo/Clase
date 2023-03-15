622ef6312e9c7bd502d5b0c7a4d66c931f458355
Rubenyrayo <Rubenyrayo@hotmail.com>
2023-03-15 09:15:35 +0100

    a

---
+++ Programacion/Programación_final_tkinter/Test.py
@@ -0,0 +1,23 @@
+"""
+Novela grafica donde puedes tomar decisiones a parte, también añadir momentos de pelea donde puedas mejorar a tu personaje
+
+@Autor: Ruben Delgado
+@Mail: neburyj4@gmail.com
+@Data de creació: 15/03/2023
+@Data de modificació: 15/03/2023
+@Versio 1.0
+
+"""
+
+import tkinter as tk
+from tkinter import *
+
+w=Tk()
+w.geometry('900x500')
+w.configure(bg='#262626')
+w.resizable(0,0)
+w.title('Hola')
+
+
+    
+w.mainloop()
